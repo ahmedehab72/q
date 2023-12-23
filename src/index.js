d924a540900ed1af5750364e076d8dd1c29c4649
ahmedehab72 <a01011618272@gmail.com>
2023-12-23 13:07:59 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM  from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+import info from `./package.json` assert { type: `json` };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
